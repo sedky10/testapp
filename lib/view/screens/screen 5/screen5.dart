064f110783200d00ec739cc41cb36f15e61b3b17
Ebrahim Sayedelahl <111066473+Ebrahimsayedelahl@users.noreply.github.com>
2023-10-21 17:34:10 +0300

    screeen 5

--- lib/view/screens/screen 5/screen5.dart
+++ lib/view/screens/screen 5/screen5.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-class Screen1 extends StatelessWidget {
-  const Screen1({super.key});
+class Screen5 extends StatelessWidget {
+  const Screen5({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:  Container(
+        decoration: BoxDecoration(
+          color: Colors.lightGreenAccent,
+        ),
+        child: Column(
+          children: [
+            Stack(children: [
+
+        Image.asset('lib/view/screens/screen 5/images/photo.jpg'),
+              SizedBox(height: 500,),
+              Row(
+                children: [
+                  SizedBox(width: 90,),
+                  Icon(Icons.share,size: 30,),
+                  SizedBox(width: 30,),
+                  Icon(Icons.favorite_border_rounded,size: 30,),
+                ],
+              ),
+
+            ],
+            )
+          ],
+        ),
+      )
+    );
   }
 }

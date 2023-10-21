badee5ca3554523f691eadd7f0741165e4a4a7b4
bob <bob@>
2023-10-21 16:55:10 +0300

    versu-ion 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testapp/view/screens/screen%201/screen1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: Screen1(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/view/screens/screen 1/screen1.dart
+++ lib/view/screens/screen 1/screen1.dart
@@ -5,6 +5,30 @@ class Screen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text('VPN',style: TextStyle(color: Colors.black ),),
+          leading: const Icon(Icons.menu,color: Colors.black,),
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {},
+                  child: const Row(
+                    children: [
+                      Text("Disconnect", style: TextStyle(color: Colors.grey),),
+
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

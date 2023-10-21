1e38606871565ff50e15d8beec7d1197d4ed1380
Mahmoud-Niazy <mahmoudniazy33@gmail.com>
2023-10-21 17:04:05 +0300

    'Screen 9 '

--- lib/view/screens/screen 9/screen9.dart
+++ lib/view/screens/screen 9/screen9.dart
@@ -5,6 +5,29 @@ class Screen9 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            'https://media.istockphoto.com/id/1173300976/vector/smile-face-icon-isolated-on-white-background-vector-illustration.jpg?s=612x612&w=0&k=20&c=x-NfEzazlDkypdO4CBbYDPJoTAOTDVFasAZy2mrDH4U=',
+          fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              children: [
+                Text(
+                  'How was your day ?',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

41be091da3971a44774b28a6981e1af5499dfeda
shimaa <shimaafoda@std.mans.edu.eg>
2023-10-21 17:09:29 +0200

    screen two

--- lib/view/screens/screen 2/screen2.dart
+++ lib/view/screens/screen 2/screen2.dart
@@ -1,10 +1,29 @@
+
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
-  const Screen2({super.key});
+class ScreenTwo extends StatelessWidget {
+  const ScreenTwo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("ENJOY READING",style: TextStyle(color: Colors.black,fontSize: 34),),
+            ),
+            Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+              child: Row(children: [
+              Text("search for books",style: TextStyle(color: Colors.grey,fontSize: 19,),),
+              SizedBox(width: 20,),
+              IconButton(onPressed: (){}, icon: Icon(Icons.search,size: 20,)),
+            ],),)
+            
+          ],
+        ),
+      ),
+    );
   }
 }

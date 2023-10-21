b6885c3d9504c9d12377b376659f811d7b097b6a
mahmoud sherif <mahmsherif28@gmail.com>
2023-10-21 17:23:50 +0300

    done

--- lib/view/screens/screen 8/screen8.dart
+++ lib/view/screens/screen 8/screen8.dart
@@ -6,11 +6,62 @@ class Screen8 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
+      body: Column(
         children: [
-          Image.network("https://tse4.mm.bing.net/th?id=OIP.gk1FZ3M8sKwdwYNIk_CpqAD6D5&pid=Api&P=0&h=220"),
-          SizedBox(height: 10,),
-          Text("verify your email"),
+          Image.network(
+              "https://tse4.mm.bing.net/th?id=OIP.gk1FZ3M8sKwdwYNIk_CpqAD6D5&pid=Api&P=0&h=220"),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "verify your email",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "please enter the 4 digit code sent to",
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400),
+          ),
+          Text(
+            "mahmosherif28@gmail",
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "9",
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "resend code",
+              style: TextStyle(color: Color(0xff6A52F0)),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Color(0xff6A52F0))),
+            onPressed: () {},
+            child: Text(
+              "confirm",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "change email",
+              style: TextStyle(color: Color(0xff6A52F0)),
+            ),
+          ),
         ],
       ),
     );

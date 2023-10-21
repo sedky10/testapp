81f2b58ba2db8d42ba5327734df797917f6f9958
Abdelrahman Sobhy <bodeysobhy@gmail.com>
2023-10-21 17:29:01 +0300

    first commit by sobhy screen 10

--- lib/view/screens/screen 10/screen10.dart
+++ lib/view/screens/screen 10/screen10.dart
@@ -5,6 +5,41 @@ class Screen10 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Icon(Icons.person , color: Colors.blue,),
+        leading: const Icon(Icons.notifications_active , color: Colors.black,),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: const [
+                  Text("Current Balance", style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),),
+                  Text("Cash Available" ,  style: TextStyle(
+                  color: Colors.grey,
+                      fontSize: 30,
+                  ),),
+                ],
+              ), 
+              const SizedBox(width: 30,),
+              const Text("\$15,382" , style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),),
+              ],
+          )
+        ],
+      ),
+    );
   }
 }

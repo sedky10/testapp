b2700aebc4d18cfaa5169300c132e4f806830102
sedky10 <sirsodky123@gmail.com>
2023-10-21 16:25:31 +0300

    Initial commit

---
+++ lib/view/screens/screen 3/screen3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Screen7 extends StatelessWidget {
+  const Screen7({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/view/screens/screen 8/screen8.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Screen8 extends StatelessWidget {
+  const Screen8({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

cbced44a8954789ab88492e348ea9ade288afbe5
mahmoud sherif <mahmsherif28@gmail.com>
2023-10-21 16:59:11 +0300

    xd

--- lib/view/screens/screen 8/screen8.dart
+++ lib/view/screens/screen 8/screen8.dart
@@ -5,6 +5,14 @@ class Screen8 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:Column(
+        children: [
+          Image.network("https://tse4.mm.bing.net/th?id=OIP.gk1FZ3M8sKwdwYNIk_CpqAD6D5&pid=Api&P=0&h=220"),
+          SizedBox(height: 10,),
+          Text("verify your email"),
+        ],
+      ),
+    );
   }
 }

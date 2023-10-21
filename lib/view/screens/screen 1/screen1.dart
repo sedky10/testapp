29bc16ecab4981a1234cf411ffcbd97e29b9cd03
bob <bob@>
2023-10-21 17:22:01 +0300

    versu-ion 3

--- lib/view/screens/screen 1/screen1.dart
+++ lib/view/screens/screen 1/screen1.dart
@@ -18,22 +18,57 @@ class Screen1 extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
+              SizedBox(
                 width: 200,
                 child: ElevatedButton(onPressed: () {},
                     style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20)
                       ),
-                      backgroundColor: Colors.white,
                     ),
                     child: const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text("Disconnect", style: TextStyle(color: Colors.grey),),
+                        SizedBox(width: 10,),
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          minRadius: 5 ,)
                       ],
                     )),
-              )
+              ),
+              const SizedBox(height: 30,),
+              const CircleAvatar(
+                backgroundColor: Colors.grey,
+                maxRadius: 90,
+                child: Icon(Icons.arrow_forward,size: 80,color: Colors.white,),
+              ),
+              const SizedBox(height: 30,),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("CONNECT NOW", style: TextStyle(color: Colors.white),),
+                      ],
+                    )),
+              ),
+              SizedBox(
+                child: Row(
+                  children: [
+                    CircleAvatar(
+
+                    )
+                  ],
+                ) ,)
             ],
           ),
         ),
